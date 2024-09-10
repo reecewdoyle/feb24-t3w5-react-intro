65ad20ed7bb8bd61fa4fd3159d205ac865eb8ec9
reecewdoyle <144601273+reecewdoyle@users.noreply.github.com>
2024-09-10 20:33:36 +1000

    feat: init react app

---
+++ src/App.js
@@ -0,0 +1,34 @@
+
+import './App.css';
+
+const food = [
+  "bananas",
+  "bacon",
+  "pizza",
+  "mango",
+  "nachos",
+  "KFC",
+  "sushi",
+  "tinned spaghetti"
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world!</h1>
+
+      <ul>
+        {
+          food.map(individualFoodItem => {
+            return <li>
+              {individualFoodItem}
+            </li>
+          })
+        }
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
